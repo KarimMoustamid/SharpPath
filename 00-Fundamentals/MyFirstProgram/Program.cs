8bff120b58cab4b020b6ae15446fcec5d6655bdb
karimmoustamid <karim.moustamid@agilethought.com>
2024-10-21 10:52:44 -0600

    Add initial project setup for "Fundamentals"
    Created the initial Visual Studio solution for "Fundamentals". Added necessary configuration files including .gitignore, encoding settings, and VCS mappings. Implemented a basic UnitConverter program in C#.

---
+++ 00-Fundamentals/MyFirstProgram/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+#region Syntax
+
+UnitConverter feetToInchesConverter = new UnitConverter(12);
+Console.WriteLine(feetToInchesConverter.Convert(30));
+
+public class UnitConverter
+{
+    private int ratio;
+
+    public UnitConverter(int unitRatio)
+    {
+        ratio = unitRatio;
+    }
+
+    public int Convert(int unit)
+    {
+        return ratio * unit;
+    }
+}
+
+#endregion
